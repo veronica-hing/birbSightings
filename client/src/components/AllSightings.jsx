5d9660e8c7acbf25b4280e115a97fb6d5649a508
veronica-hing <veronica.m.hing@gmail.com>
2022-12-09 20:09:06 -0800

    styled the list a bit more

--- client/src/components/AllSightings.jsx
+++ client/src/components/AllSightings.jsx
@@ -17,23 +17,25 @@ const AllSightings = (props) => {
     return (
         <div>
             <h3>Bird Sightings so far:</h3>
+            <div className='d-inline-flex flex-column'>
             {
                 alphabetic.map((item)=>{
                     return(
-                        <div className='mx-5 d-flex flex-row justify-content-center border border-dark rounded-3'>
-                            <div>
-                                <h2>{item.location}</h2>
-                                <p>Location: {item.location}</p>
-                                <p>Date: {item.date}</p>
+                        
+                            <div className='p-2 d-flex justify-content-left border border-dark rounded-3'>
+                                <div>
+                                    <p>Location: {item.location} - Date: {item.date}</p>
+                                    <p>Reported by: {item.reporter}</p>
+                                </div>
+                                <div className="p-4 d-flex justify-content-right align-items-center">
+                                    <button onClick = {e=>deleteSighting(item._id)} className = "btn btn-danger mx-2">Delete</button>
+                                    < Link to={"/sightings/" + item._id + "/edit"} type ="button" className = "btn btn-primary mx-2">Edit/View</Link>
+                                </div>
                             </div>
-                            <div className="d-flex align-items-center">
-                                <button onClick = {e=>deleteSighting(item._id)} className = "btn btn-danger mx-2">Delete</button>
-                                < Link to={"/sightings/" + item._id + "/edit"} type ="button" className = "btn btn-primary mx-2">Edit/View</Link>
-                            </div>
-                        </div>
                     );
                 })
             }
+            </div>
         </div>
     );
 };
